5fbfaa3cf52943ee334cada600d06f78ee61ac73
siykt <cnsiykt@163.com>
2023-06-05 13:28:55 +0800

    feat: logo

--- components/Layout.vue
+++ components/Layout.vue
@@ -2,16 +2,20 @@
 // something
 </script>
 <template>
-  <div class="w-screen h-screen relative flex">
+  <div class="w-screen h-screen relative flex content">
     <div class="absolute top-0 left-0 bottom-0 right-0 bg bg-center bg-cover w-screen h-screen"></div>
     <div class="absolute top-0 left-0 bottom-0 right-0 bg-gray-800 bg-opacity-75 w-screen h-screen"></div>
-    <div class="flex m-auto bg-white w-screen h-screen sm:flex-col md:flex-row md:h-auto md:max-w-5xl md:rounded-md relative z-1">
+    <div class="flex mx-auto my-15vw bg-white w-screen h-screen flex-col md:h-auto md:w-3xl md:rounded-md relative z-1 overflow-hidden">
       <slot />
     </div>
   </div>
 </template>
 
 <style lang="less" scoped>
+// @import url('https://fonts.googleapis.com/css2?family=Playfair+Display&display=swap');
+.content {
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, Arial, sans-serif;
+}
 .bg {
   background-image: url(/assets/bg.png);
   filter: blur(20px);
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
-const { username = 'AntPro' } = useAppConfig();
+import LogoPNG from '@/assets/logo.png';
 </script>
 <template>
-  <div class="p3">
-    <h1 class="text-lg flex items-center">
-      <span>{{ username }}-TODO</span>
-    </h1>
+  <h1 class="p2 flex items-center m0 text-xl font-medium text-#333 bg-#e7eaed">
+    <img class="w-60px h-60px mr-6px" :src="LogoPNG" alt="logo.png" />
+    <span>TODO</span>
+  </h1>
+  <div class="p2 flex flex-1">
+    <Side />
   </div>
 </template>
