e6e880c96bf176383518b3668f1c6c2fcb1c9777
siykt <cnsiykt@163.com>
2023-06-05 13:34:42 +0800

    feat: Bowers storage

---
+++ lib/storage.ts
@@ -0,0 +1,35 @@
+import { createInstance } from 'localforage';
+
+class Storage {
+  private store: LocalForage;
+
+  constructor() {
+    this.store = createInstance({
+      name: 'MP_TODO',
+      storeName: 'todos',
+    });
+  }
+
+  async get<T = any>(key: string, def?: T) {
+    return (await this.store.getItem<T>(key)) ?? (def as T);
+  }
+
+  async set(key: string, value: any) {
+    return await this.store.setItem(key, value);
+  }
+
+  async remove(key: string) {
+    return await this.store.removeItem(key);
+  }
+
+  async clear() {
+    return await this.store.clear();
+  }
+
+  async length() {
+    return await this.store.length();
+  }
+}
+
+const storage = new Storage();
+export default storage;
