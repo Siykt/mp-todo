619ed50bcb60bf95dfd5a7fe1bfe1064fbd6c8f1
siykt <cnsiykt@163.com>
2023-06-07 14:54:25 +0800

    feat: 优化邮件样式

--- server/api/mail.ts
+++ server/api/mail.ts
@@ -4,7 +4,7 @@ import { Settings } from '../../modules/Settings/Core';
 
 interface MailBody {
   to: string;
-  data: object;
+  data: { message: string; title: string };
   attachments?: Attachment[];
   config: Settings;
 }
@@ -33,16 +33,71 @@ async function sendMail(body: MailBody) {
     subject: '来自您的 TODO 消息提示',
     from: `TODO消息助手 <${SMTPFrom}>`,
     html: `
-      <div style="padding: 20px; background-color: #f5f5f5;">
-        <div style="padding: 20px; background-color: #fff; border-radius: 4px;">
-          ${data}
+      <head>
+        <style>
+          .card {
+            position: relative;
+            max-width: 640px;
+            min-height: 200px;
+            margin: 0 auto;
+            border: 2px solid #e9e9e9;
+            border-radius: 8px;
+            margin-bottom: 14px;
+            overflow: hidden;
+          }
+          .card .title {
+            display: flex;
+            align-items: center;
+            background: #f0f5ff;
+            font-weight: bold;
+            font-size: 14px;
+            color: #6c6cc9;
+            padding: 12px 28px;
+          }
+          .card .body {
+            padding: 12px 28px;
+            position: relative;
+            background-color: #fff;
+          }
+          .card .body p {
+            margin: 0;
+            margin-bottom: 12px;
+            font-size: 14px;
+            color: #666;
+            line-height: 1.5;
+            word-wrap: break-word;
+            word-break: break-all;
+            white-space: pre-wrap;
+          }
+          .card a {
+            color: #6c6cc9;
+            text-decoration: none;
+          }
+          .author {
+            margin: 0 12px 12px auto;
+            width: fit-content;
+            font-size: 12px;
+            color: #999;
+          }
+        </style>
+      </head>
+      <div class="card">
+        <div class="title">
+          <span>TODO消息助手</span>
+          <a href="https://todo.antpro.me" target="_blank" style="margin-left: auto;">查看详情</a>
         </div>
+        <div class="body">
+          <p>您有一条来自${data.title}新的消息，请查收。</p>
+          <p>消息内容如下：</p>
+          <div class="body" style="margin-bottom: 20px;">
+            <p>${data.message}</p>
+          </div>
+        </div>
+        <div class="author">来自 TODO 消息助手 By <a href="https://github.com/Siykt" target="_blank">Siykt</a></div>
       </div>
     `,
     attachments,
   });
-
-  console.log('Message sent: ', info);
   return info;
 }
 
