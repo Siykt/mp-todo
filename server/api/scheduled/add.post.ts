b00fcb9314b4455c387902ba4243698231147e76
siykt <cnsiykt@163.com>
2023-06-07 17:13:56 +0800

    fix: post

--- server/api/scheduled/add.post.ts
+++ server/api/scheduled/add.post.ts
@@ -13,5 +13,5 @@ export default defineEventHandler(async event => {
   // !fix 使用环境检查而不是URL检查
   if (url.includes('192.168') || url.includes('localhost') || url.includes('127.0.0.1')) url = 'https://todo.antpro.me';
   if (url.endsWith('/')) url = url.slice(0, -1);
-  return addScheduled({ authorization, URL: url, delay, cron, maxRetry, body });
+  return addScheduled({ authorization, URL: `${url}/api/mail`, delay, cron, maxRetry, body });
 });
