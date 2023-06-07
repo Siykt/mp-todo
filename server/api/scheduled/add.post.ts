18c6bb23292695775606d81cd66cde641ea61b39
siykt <cnsiykt@163.com>
2023-06-07 17:15:40 +0800

    fix: params

--- server/api/scheduled/add.post.ts
+++ server/api/scheduled/add.post.ts
@@ -3,15 +3,14 @@ import { addScheduled } from '../../external/QStashAPI';
 
 export default defineEventHandler(async event => {
   const { QSTASH_AUTHORIZATION } = useRuntimeConfig();
-  const { authorization = QSTASH_AUTHORIZATION, delay, cron, maxRetry, body } = (await readBody<AddScheduledConfig>(event)) ?? {};
+  const { authorization = QSTASH_AUTHORIZATION, URL, ...others } = (await readBody<AddScheduledConfig>(event)) ?? {};
 
   if (!authorization) throw new Error('Authorization不能为空');
 
   const headers = getHeaders(event);
-  const { URL } = useRuntimeConfig();
   let url: string = URL ?? headers.origin ?? 'https://todo.antpro.me';
   // !fix 使用环境检查而不是URL检查
   if (url.includes('192.168') || url.includes('localhost') || url.includes('127.0.0.1')) url = 'https://todo.antpro.me';
   if (url.endsWith('/')) url = url.slice(0, -1);
-  return addScheduled({ authorization, URL: `${url}/api/mail`, delay, cron, maxRetry, body });
+  return addScheduled({ authorization, URL: `${url}/api/mail`, ...others });
 });
