2293f80ece7b5e890296eb29d98de7d4dcbe49ea
siykt <cnsiykt@163.com>
2023-06-28 21:07:41 +0800

    feat: 更多配置

--- server/external/Authing.ts
+++ server/external/Authing.ts
@@ -1,12 +1,12 @@
 import { AuthenticationClient } from 'authing-js-sdk'
 
-const { AUTHING } = useRuntimeConfig()
+const { AUTHING, NUXT_APP_DOMAIN } = useRuntimeConfig()
 
 const authingClient = new AuthenticationClient({
   appId: AUTHING.APP_ID,
   secret: AUTHING.APP_SECRET,
   appHost: `https://${AUTHING.ISSUER}.authing.cn`,
-  redirectUri: 'http://localhost:3000/auth/callback',
+  redirectUri: `${NUXT_APP_DOMAIN}/auth/callback`,
 })
 
 export { authingClient }
